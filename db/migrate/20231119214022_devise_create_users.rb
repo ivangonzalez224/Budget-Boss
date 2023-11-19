3d1920cb7b123c562bdf244def8dec9a6ca6dc81
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-19 17:08:44 -0500

    Create the users table

--- db/migrate/20231119214022_devise_create_users.rb
+++ db/migrate/20231119214022_devise_create_users.rb
@@ -3,6 +3,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
+      t.string :name, null: false
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -22,10 +23,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       # t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -38,7 +39,23 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+  end
+
+  def self.down
+    remove_index :users, :confirmation_token if index_exists?(:users, :confirmation_token)
+    remove_index :users, :reset_password_token if index_exists?(:users, :reset_password_token)
+    remove_index :users, :email if index_exists?(:users, :email)
+
+    change_table :users do |t|
+      t.remove :encrypted_password if column_exists?(:users, :encrypted_password)
+      t.remove :reset_password_token if column_exists?(:users, :reset_password_token)
+      t.remove :reset_password_sent_at if column_exists?(:users, :reset_password_sent_at)
+      t.remove :remember_created_at if column_exists?(:users, :remember_created_at)
+      t.remove :confirmation_token if column_exists?(:users, :confirmation_token)
+      t.remove :confirmed_at if column_exists?(:users, :confirmed_at)
+      t.remove :confirmation_sent_at if column_exists?(:users, :confirmation_sent_at)
+    end
   end
 end
