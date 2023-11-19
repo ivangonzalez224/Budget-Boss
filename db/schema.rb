a8479a7c0c5a49226efac729a1b62471e92c16a5
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-19 17:59:11 -0500

    Create the category_acquisitions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_19_224842) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_19_225310) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_19_224842) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
+  create_table "category_acquisitions", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "acquisition_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["acquisition_id"], name: "index_category_acquisitions_on_acquisition_id"
+    t.index ["category_id"], name: "index_category_acquisitions_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -53,4 +62,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_19_224842) do
 
   add_foreign_key "acquisitions", "users", column: "author_id"
   add_foreign_key "categories", "users"
+  add_foreign_key "category_acquisitions", "acquisitions"
+  add_foreign_key "category_acquisitions", "categories"
 end
