8d01c795f2cc30db3c1a0954741e3e564ab65c97
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-19 17:52:08 -0500

    Create the acquisitions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_19_221156) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_19_224842) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "acquisitions", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount", precision: 10, scale: 1
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_acquisitions_on_author_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -42,5 +51,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_19_221156) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "acquisitions", "users", column: "author_id"
   add_foreign_key "categories", "users"
 end
