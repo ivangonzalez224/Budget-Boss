5843ee2e42819b021ea34efda6a04c1c55b018dc
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-20 11:24:58 -0500

    Create the splash view structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,9 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "splash#index"
-  
   resources :splash, only: [:index]
-  resources :home, only: [:index, :new, :create]
+  resources :categories, only: [:index, :new, :create]
   resources :acquisition, only: [:show, :new, :create]
   
 end
