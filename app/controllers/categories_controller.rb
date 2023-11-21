6fab3fbec6f12fc601a4be866388a6a1d5f0632a
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-20 21:59:13 -0500

    Add the categories view structure andd styles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-   @categories = Category.where(user_id: current_user.id)
-   @current_user = current_user
+    @categories = Category.where(user_id: current_user.id)
+    @current_user = current_user
   end
-
 end
