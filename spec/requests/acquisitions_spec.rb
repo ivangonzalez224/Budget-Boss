b6087530649400a2e4e20c0535d19e3b78c87ad4
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-21 19:56:17 -0500

    Test the acqusition controller

--- spec/requests/acquisitions_spec.rb
+++ spec/requests/acquisitions_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'CategoryController', type: :request do
+RSpec.describe 'AcquisitionController', type: :request do
   describe 'GET #index' do
     it 'returns a successful response' do
-      get '/'
+      get '/acquisition/1'
       expect(response).to have_http_status(302)
     end
 
     it 'returns an error response for a wrong path' do
-      get '/failed_path'
+      get '/acquisition'
       expect(response).to have_http_status(404)
     end
 
     it 'should redirects to the sign in page for an unauthenticated user' do
-      get '/categories'
+      get '/acquisition/2'
       expect(response).to redirect_to(new_user_session_path)
     end
   end
